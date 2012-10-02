4cc10be3550b790811cb70d9bbd8e2c322de00da
Duncan Bayne <duncan.bayne@lonelyplanet.com.au>
2012-10-02 12:12:28 +1000

    Pass max_nesting option into JSON libs by default (fix for #58).

--- lib/multi_json/adapters/json_common.rb
+++ lib/multi_json/adapters/json_common.rb
@@ -4,7 +4,7 @@ module MultiJson
 
       def load(string, options={})
         string = string.read if string.respond_to?(:read)
-        ::JSON.parse(string, :symbolize_names => options[:symbolize_keys])
+        ::JSON.parse(string, :symbolize_names => options[:symbolize_keys], :max_nesting => options[:max_nesting])
       end
 
       def dump(object, options={})
