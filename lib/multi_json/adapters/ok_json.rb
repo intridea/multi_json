b2b6b4525fea3cf7878e68691284503f8d1520b7
Pavel Pravosud <pavel@pravosud.com>
2012-12-11 15:02:46 +0700

    OkJson is a module

--- lib/multi_json/adapters/ok_json.rb
+++ lib/multi_json/adapters/ok_json.rb
@@ -2,32 +2,34 @@ require 'multi_json/vendor/okjson'
 
 module MultiJson
   module Adapters
-    class OkJson
+    module OkJson
+      extend self
+
       ParseError = ::MultiJson::OkJson::Error
 
-      def self.load(string, options={}) #:nodoc:
+      def load(string, options={}) #:nodoc:
         string = string.read if string.respond_to?(:read)
         result = ::MultiJson::OkJson.decode("[#{string}]").first
         options[:symbolize_keys] ? symbolize_keys(result) : result
       end
 
-      def self.dump(object, options={}) #:nodoc:
+      def dump(object, options={}) #:nodoc:
         ::MultiJson::OkJson.valenc(stringify_keys(object))
       end
 
-      def self.symbolize_keys(object) #:nodoc:
+      def symbolize_keys(object) #:nodoc:
         modify_keys(object) do |key|
           key.is_a?(String) ? key.to_sym : key
         end
       end
 
-      def self.stringify_keys(object) #:nodoc:
+      def stringify_keys(object) #:nodoc:
         modify_keys(object) do |key|
           key.respond_to?(:to_s) ? key.to_s : key
         end
       end
 
-      def self.modify_keys(object, &modifier) #:nodoc:
+      def modify_keys(object, &modifier) #:nodoc:
         case object
         when Array
           object.map do |value|
