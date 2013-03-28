8c6131f2fd604c3aa8118e5bb708f201e27b34c7
Pavel Pravosud <pavel@pravosud.com>
2013-03-28 19:19:53 +0700

    Use json/pure to test adapters do not modify options

--- spec/adapter_shared_example.rb
+++ spec/adapter_shared_example.rb
@@ -13,7 +13,7 @@ shared_examples_for 'an adapter' do |adapter|
   it_behaves_like 'has options', lambda{ MultiJson.adapter }
 
   it 'does not modify argument hashes' do
-    options = { :symbolize_keys => true, :pretty => false, :adapter => :json_gem }
+    options = { :symbolize_keys => true, :pretty => false, :adapter => :json_pure }
     expect{MultiJson.load('{}', options)}.to_not change{options}
     expect{MultiJson.dump([42], options)}.to_not change{options}
   end
