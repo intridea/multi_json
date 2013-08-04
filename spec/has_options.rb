3566308ec07b2598bd2f7011a1a44ddc2b7e0479
Erik Michaels-Ober <sferik@gmail.com>
2013-08-04 09:31:52 +0200

    WET specs
    https://twitter.com/sferik/status/361880259316027393

--- spec/has_options.rb
+++ spec/has_options.rb
@@ -6,69 +6,114 @@ shared_examples_for 'has options' do |object|
     subject{ object }
   end
 
-  %w(dump_options load_options).each do |getter|
+  describe "dump options" do
 
-    let(:getter){ getter }
-    let(:default_getter){ "default_#{getter}" }
-    let(:setter){ "#{getter}=" }
-    let(:defaults){ subject.send(default_getter) }
-    let(:ivar){ "@#{getter}" }
+    before do
+      subject.dump_options = nil
+    end
 
-    describe getter.tr('_', ' ') do
-      before{ set nil }
-      after{ set nil }
+    after do
+      subject.dump_options = nil
+    end
 
-      def get(*args)
-        subject.send(getter, *args)
-      end
+    it 'returns default options if not set' do
+      expect(subject.dump_options).to eq(subject.default_dump_options)
+    end
 
-      def set(value)
-        subject.send(setter, value)
-      end
+    it 'allows hashes' do
+      subject.dump_options = {:foo => 'bar'}
+      expect(subject.dump_options).to eq(:foo => 'bar')
+    end
 
-      it 'returns default options if not set' do
-        expect(get).to eq(defaults)
-      end
+    it 'allows objects that implement #to_hash' do
+      value = Class.new do
+        def to_hash
+          {:foo => 'bar'}
+        end
+      end.new
 
-      it 'allows hashes' do
-        set :foo => 'bar'
-        expect(get).to eq(:foo => 'bar')
-      end
+      subject.dump_options = value
+      expect(subject.dump_options).to eq(:foo => 'bar')
+    end
 
-      it 'allows objects that implement #to_hash' do
-        value = Class.new do
-          def to_hash
-            {:foo=>'bar'}
-          end
-        end.new
+    it 'evaluates lambda returning options (with args)' do
+      subject.dump_options = lambda{ |a1, a2| { a1 => a2 }}
+      expect(subject.dump_options('1', '2')).to eq('1' => '2')
+    end
 
-        set value
-        expect(get).to eq(:foo => 'bar')
-      end
+    it 'evaluates lambda returning options (with no args)' do
+      subject.dump_options = lambda{{:foo => 'bar'}}
+      expect(subject.dump_options).to eq(:foo => 'bar')
+    end
 
-      it 'evaluates lambda returning options (with args)' do
-        set lambda{ |a1, a2| { a1 => a2 }}
-        expect(get('1', '2')).to eq('1' => '2')
-      end
+    it 'returns empty hash in all other cases' do
+      subject.dump_options = true
+      expect(subject.dump_options).to eq(subject.default_dump_options)
 
-      it 'evaluates lambda returning options (with no args)' do
-        set lambda{{:foo => 'bar'}}
-        expect(get).to eq(:foo => 'bar')
-      end
+      subject.dump_options = false
+      expect(subject.dump_options).to eq(subject.default_dump_options)
 
-      it 'returns empty hash in all other cases' do
-        set true
-        expect(get).to eq(defaults)
+      subject.dump_options = 10
+      expect(subject.dump_options).to eq(subject.default_dump_options)
 
-        set false
-        expect(get).to eq(defaults)
-
-        set 10
-        expect(get).to eq(defaults)
-
-        set nil
-        expect(get).to eq(defaults)
-      end
+      subject.dump_options = nil
+      expect(subject.dump_options).to eq(subject.default_dump_options)
     end
   end
+
+  describe "load options" do
+
+    before do
+      subject.load_options = nil
+    end
+
+    after do
+      subject.load_options = nil
+    end
+
+    it 'returns default options if not set' do
+      expect(subject.load_options).to eq(subject.default_load_options)
+    end
+
+    it 'allows hashes' do
+      subject.load_options = {:foo => 'bar'}
+      expect(subject.load_options).to eq(:foo => 'bar')
+    end
+
+    it 'allows objects that implement #to_hash' do
+      value = Class.new do
+        def to_hash
+          {:foo => 'bar'}
+        end
+      end.new
+
+      subject.load_options = value
+      expect(subject.load_options).to eq(:foo => 'bar')
+    end
+
+    it 'evaluates lambda returning options (with args)' do
+      subject.load_options = lambda{ |a1, a2| { a1 => a2 }}
+      expect(subject.load_options('1', '2')).to eq('1' => '2')
+    end
+
+    it 'evaluates lambda returning options (with no args)' do
+      subject.load_options = lambda{{:foo => 'bar'}}
+      expect(subject.load_options).to eq(:foo => 'bar')
+    end
+
+    it 'returns empty hash in all other cases' do
+      subject.load_options = true
+      expect(subject.load_options).to eq(subject.default_load_options)
+
+      subject.load_options = false
+      expect(subject.load_options).to eq(subject.default_load_options)
+
+      subject.load_options = 10
+      expect(subject.load_options).to eq(subject.default_load_options)
+
+      subject.load_options = nil
+      expect(subject.load_options).to eq(subject.default_load_options)
+    end
+  end
+
 end
