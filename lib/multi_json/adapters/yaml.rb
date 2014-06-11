d3fdcb00acb524ce8ed2a95e644acee346a7c530
Kenneth Rose <ken@pagerduty.com>
2014-06-10 17:10:10 -0700

    Catch YAML::SyntaxError, not just SyntaxError.
    In new versions of Psych, YAML::SyntaxError (which is
    Psych::SyntaxError) is no longer a subclass of ::SyntaxError.

--- lib/multi_json/adapters/yaml.rb
+++ lib/multi_json/adapters/yaml.rb
@@ -1,4 +1,5 @@
 require 'multi_json/adapters/json_common'
+require 'yaml'
 
 module MultiJson
   module Adapters
@@ -7,7 +8,7 @@ module MultiJson
     class Yaml
       extend JsonCommon
 
-      ParseError = SyntaxError
+      ParseError = ::YAML::SyntaxError
       DATE_REGEX = /^(?:\d{4}-\d{2}-\d{2}|\d{4}-\d{1,2}-\d{1,2}[ \t]+\d{1,2}:\d{2}:\d{2}(\.[0-9]*)?(([ \t]*)Z|[-+]\d{2}?(:\d{2})?))$/
       
 
